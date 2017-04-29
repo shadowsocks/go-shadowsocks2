5ed8f0a58f431d69253d299cbeee335ee44d6097
Yifu Yu <root@jackyyf.com>
2017-04-29 17:54:58 +0800

    Added dialer interface for golang.org/x/net/proxy.
    Need to be tested, I'll add some simple test.

---
+++ core/dialer.go
@@ -0,0 +1,52 @@
+package core
+
+import (
+	"net"
+	"errors"
+	"github.com/shadowsocks/go-shadowsocks2/socks"
+)
+
+type ProxyDialer struct {
+	StreamConnCipher
+	addr string
+}
+
+func NewDialer(addr string, algo string, password string) (s *ProxyDialer, err error) {
+	cipher, err := PickCipher(algo, nil, password)
+	if err != nil {
+		return
+	}
+	s = new(ProxyDialer)
+	s.StreamConnCipher = cipher
+	s.addr = addr
+	return
+}
+
+func NewDialerFromKey(addr string, algo string, key []byte) (s *ProxyDialer, err error) {
+	cipher, err := PickCipher(algo, key, "")
+	if err != nil {
+		return
+	}
+	s = new(ProxyDialer)
+	s.StreamConnCipher = cipher
+	s.addr = addr
+	return
+}
+
+func (dialer *ProxyDialer) Dial(network, addr string) (c net.Conn, err error) {
+	target := socks.ParseAddr(addr)
+	if target == nil {
+		return nil, errors.New("Unable to parse address: " + addr)
+	}
+	conn, err := net.Dial("tcp", dialer.addr)
+	if err != nil {
+		return nil, err
+	}
+	conn.(*net.TCPConn).SetKeepAlive(true)
+	c = dialer.StreamConn(conn)
+	if _, err = c.Write(target); err != nil {
+		c.Close()
+		return nil, err
+	}
+	return
+}
