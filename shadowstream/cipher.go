e4ac9099a66999ba418771c58a3883b041399450
ksafe <ksafer@gmail.com>
2017-08-13 22:27:10 +0800

    Added Chacha20 & Hchacha20 stream cipher

--- shadowstream/cipher.go
+++ shadowstream/cipher.go
@@ -51,6 +51,26 @@ func AESCFB(key []byte) (Cipher, error) {
 	return &cfbStream{blk}, nil
 }
 
+// Chacha20
+type chacha20key []byte
+
+func (k chacha20key) IVSize() int                       { return chacha20.NonceSize }
+func (k chacha20key) Decrypter(iv []byte) cipher.Stream { return k.Encrypter(iv) }
+func (k chacha20key) Encrypter(iv []byte) cipher.Stream {
+	ciph, err := chacha20.NewCipher(k, iv)
+	if err != nil {
+		panic(err) // should never happen
+	}
+	return ciph
+}
+
+func Chacha20(key []byte) (Cipher, error) {
+	if len(key) != chacha20.KeySize {
+		return nil, KeySizeError(chacha20.KeySize)
+	}
+	return chacha20key(key), nil
+}
+
 // IETF-variant of chacha20
 type chacha20ietfkey []byte
 
@@ -71,6 +91,7 @@ func Chacha20IETF(key []byte) (Cipher, error) {
 	return chacha20ietfkey(key), nil
 }
 
+// xchacha20
 type xchacha20key []byte
 
 func (k xchacha20key) IVSize() int                       { return chacha20.XNonceSize }
@@ -88,4 +109,24 @@ func Xchacha20(key []byte) (Cipher, error) {
 		return nil, KeySizeError(chacha20.KeySize)
 	}
 	return xchacha20key(key), nil
+}
+
+// Hchacha20
+type hchacha20key []byte
+
+func (k hchacha20key) IVSize() int                       { return chacha20.HNonceSize }
+func (k hchacha20key) Decrypter(iv []byte) cipher.Stream { return k.Encrypter(iv) }
+func (k hchacha20key) Encrypter(iv []byte) cipher.Stream {
+	ciph, err := chacha20.NewCipher(k, iv)
+	if err != nil {
+		panic(err) // should never happen
+	}
+	return ciph
+}
+
+func Hchacha20(key []byte) (Cipher, error) {
+	if len(key) != chacha20.KeySize {
+		return nil, KeySizeError(chacha20.KeySize)
+	}
+	return hchacha20key(key), nil
 }
