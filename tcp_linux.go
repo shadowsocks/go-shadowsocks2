bc3f04c445ed6c148f09efb44c764ff1988543fd
Sherlock Holo <sherlockya@gmail.com>
2018-06-07 14:52:42 +0800

    Change repo to mine

--- tcp_linux.go
+++ tcp_linux.go
@@ -6,7 +6,7 @@ import (
 	"syscall"
 	"unsafe"
 
-	"github.com/shadowsocks/go-shadowsocks2/socks"
+	"github.com/Sherlock-Holo/go-shadowsocks2/socks"
 )
 
 const (
--- udp.go
+++ udp.go
@@ -7,7 +7,7 @@ import (
 
 	"sync"
 
-	"github.com/shadowsocks/go-shadowsocks2/socks"
+	"github.com/Sherlock-Holo/go-shadowsocks2/socks"
 )
 
 type mode int
